a79d8ab282233fb924cfd5d5b3b703be18e9a9fb
Diego Zarate <diego3799@gmail.com>
2021-05-02 20:49:21 -0500

    funcionando con la libreria

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from random import randint
+from sklearn.neural_network import MLPClassifier
 # Primero tenemos que pedir el numero de neuronas
 print("Numero de neuronas iniciales:")
 num_neuronas = int(input())
@@ -12,8 +13,8 @@ num_weight = num_neuronas*num_intermedias+num_intermedias
 weights = [[np.random.rand() for i in range(num_intermedias)]
            for i in range(num_neuronas)]
 
-weights += [[np.random.rand() for i in range(num_intermedias)]]
 
+weights += [[np.random.rand() for i in range(num_intermedias)]]
 
 # necesitamos generar un numero N puntos
 # con el numero de coordenas igual al numero de entradas iniciales
@@ -25,16 +26,29 @@ num_puntos = int(input())
 conjunto_datos = [[randint(0, 10) for i in range(num_neuronas)]
                   for i in range(num_puntos)]
 
+conjunto_test = [[randint(0, 10) for i in range(num_neuronas)]
+                 for i in range(num_puntos)]
+
+objetivos = []
 # Agregamos la identificacion de los puntos
 for i in range(num_puntos):
-    conjunto_datos[i].append(randint(0, 1))
+    objetivos.append(randint(0, 1))
 
 
-for i in range(num_puntos):
-    # tenemos que multiplicar por el peso de la neurona
-    acum = 0
-    hidden = []
-    # Lo que tenemos que hacer es conseguir el valor de peso de las capas intermedias
-    # que es cada uno de los pesos por las entradas
-    # despues las entradas
-    # for j in range(num_intermedias)
+print("\nPuntos Entrenamiento\n")
+print(conjunto_datos)
+print("\nObjetivos\n")
+print(objetivos)
+
+
+clf = MLPClassifier(solver='lbfgs', alpha=0.01, activation="logistic", learning_rate="constant",
+                    hidden_layer_sizes=(num_intermedias, ), random_state=1)
+
+clf.fit(conjunto_datos, objetivos)
+
+print("Resultado de prediciones")
+print(clf.predict(conjunto_datos))
+print("Resultado datos de Prueba")
+print(conjunto_test)
+print(clf.predict (conjunto_test))
+
